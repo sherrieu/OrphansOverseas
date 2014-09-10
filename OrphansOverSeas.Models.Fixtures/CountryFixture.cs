bc1b81b88bb0f258777391f7d9871040cfb1c377
Sherrie Uczekaj <sherrie.uczekaj@intel.com>
2014-09-09 21:13:49 -0700

    Adding Country and CountryTest

---
+++ OrphansOverSeas.Models.Fixtures/CountryFixture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OrphansOverSeas.Models.Fixtures
+{
+    [TestFixture]
+    public class CountryFixture
+    {
+        [Test]
+        public void CountryTest()
+        {
+            var country = new Country();
+            country.Name = "usa";
+            Assert.AreEqual("USA", country.Name);
+        }
+    }
+}
---
+++ OrphansOverSeas.Models/OrphansOverSeas.Models/Country.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace OrphansOverSeas.Models.Fixtures
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public int? Id { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? LastModified { get; set; }
+
+    }
+}
